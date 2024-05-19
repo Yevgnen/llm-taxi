c0bd0dfdc80161c1f4495d0b92bb0c5e23171516
Yevgnen Koh <wherejoystarts@gmail.com>
2024-05-19 23:23:30 +0800

    Export Embedding

--- src/llm_taxi/embeddings/__init__.py
+++ src/llm_taxi/embeddings/__init__.py
@@ -1,7 +1,9 @@
+from llm_taxi.embeddings.base import Embedding
 from llm_taxi.embeddings.mistral import MistralEmbedding
 from llm_taxi.embeddings.openai import OpenAIEmbedding
 
 __all__ = [
+    "Embedding",
     "OpenAIEmbedding",
     "MistralEmbedding",
 ]
